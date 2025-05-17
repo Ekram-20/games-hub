525ce9fd7f6b01ee42391797517f0879d538c702
Ekram Feras <ekramferasj@gmail.com>
2025-05-17 06:31:13 +0300

    optim: crop images

---
+++ src/utils/image_url.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+    // add after media/ crop parms this called crop on the fly
+    const target = "media/";
+    const index = url.indexOf(target) + target.length;
+    url = url.slice(0, index) + "crop/600/400/" + url.slice(index);
+    return url;
+}
+
+export default getCroppedImageUrl;
