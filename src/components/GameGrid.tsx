1e261a4bfdbc40ef8d81e59537ef5e76ce8c00bd
Ekram Feras <ekramferasj@gmail.com>
2025-05-17 05:21:51 +0300

    refactor: cretate custom hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,7 @@
-import api from "@/services/api";
-import { useEffect, useState } from "react";
-
-type Game = {
-  id: number;
-  name: string;
-  background_image: string;
-};
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "@/hooks/useGames";
 
 function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState<string>("");
-
-  useEffect(() => {
-    api
-      .get<FetchGameResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
