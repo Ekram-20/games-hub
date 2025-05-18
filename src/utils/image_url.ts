461359a71fe5b54ef409f49f244997da7a9101b1
Ekram Feras <ekramferasj@gmail.com>
2025-05-18 18:52:06 +0300

    fix: add default image

--- src/utils/image_url.ts
+++ src/utils/image_url.ts
@@ -1,10 +1,12 @@
+import defaultImage from "../assets/default_image.webp";
+
 const getCroppedImageUrl = (url: string) => {
-    // add after media/ crop parms this called crop on the fly
-    if (!url) return "";
-    const target = "media/";
-    const index = url.indexOf(target) + target.length;
-    url = url.slice(0, index) + "crop/600/400/" + url.slice(index);
-    return url;
-}
+  // add after media/ crop parms this called crop on the fly
+  if (!url) return defaultImage;
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  url = url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  return url;
+};
 
 export default getCroppedImageUrl;
