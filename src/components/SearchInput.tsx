2348703bc1a2ee77b62060ecb692d330873171d6
Ekram Feras <ekramferasj@gmail.com>
2025-06-10 04:21:01 +0300

    style: make the the logo clickable to home page

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,19 @@ import userGameQueryStore from "@/zustand/store";
 import { Input, InputGroup } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 function SearchInput() {
   // other option instead of state (this is better performance but more complex)
   const searchRef = useRef<HTMLInputElement>(null);
   const setSearchText = userGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (searchRef.current) {
       setSearchText(searchRef.current.value);
+      navigate("/");
     }
   };
   return (
